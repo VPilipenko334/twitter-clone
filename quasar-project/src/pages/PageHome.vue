0e4047da4328fb977ef702789e6ee7d39688c511
Veronika Pilipenko <veronika.pilipenko@jaanhealth.com>
2022-12-31 17:10:55 -0500

    added an area for the tweets

--- quasar-project/src/pages/PageHome.vue
+++ quasar-project/src/pages/PageHome.vue
@@ -2,6 +2,7 @@
   <q-page>
     <div class="q-py-lg q-px-md">
       <q-input
+        class="new-tweet"
         bottom-slots
         v-model="newTweetContent"
         placeholder="What's happening?"
@@ -26,6 +27,32 @@
             />
           </template>
         </q-input>
+
+        <q-separator inset="item"/>
+
+      <q-list>
+        <q-item clickable v-ripple>
+          <q-item-section avatar>
+            <q-avatar>
+              <img src="https://static.vecteezy.com/system/resources/previews/004/842/371/original/hipster-bear-avatar-vector.jpg">
+            </q-avatar>
+          </q-item-section>
+
+        <q-item-section>
+          <q-item-label class="text-grey-5">@veronikapilipenko</q-item-label>
+          <q-item-label>
+            <span class="text-weight-bold">Veronika</span>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere exercitationem eligendi ullam excepturi architecto, ea vitae corporis quia ducimus ex quidem inventore quis natus consectetur quas repellendus asperiores quibusdam molestias.
+          </q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          1 min ago
+        </q-item-section>
+      </q-item>
+    </q-list>
+
+
     </div>
   </q-page>
 </template>
@@ -42,3 +69,10 @@ export default defineComponent({
   }
 })
 </script>
+
+<style>
+.new-tweet {
+  font-size: 19px;
+  line-height: 1.4 !important
+}
+</style>
