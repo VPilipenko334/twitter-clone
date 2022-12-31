ae74c14557c2f9f852db0ac1e3ca8faa3c845c87
Veronika Pilipenko <veronika.pilipenko@jaanhealth.com>
2022-12-31 15:58:52 -0500

    added drawers

--- quasar-project/src/layouts/MainLayout.vue
+++ quasar-project/src/layouts/MainLayout.vue
@@ -1,55 +1,91 @@
 <template>
   <q-layout view="lHr lpR fFf">
-
-    <q-header bordered class="bg-primary text-white">
+    <q-header bordered class="bg-white text-black">
       <q-toolbar>
         <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
 
-        <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
-          </q-avatar>
-          Title
+        <q-toolbar-title class="text-weight-bold">
+          Veronika's Tweeeeeeeter Clone
         </q-toolbar-title>
-
-        <q-btn dense flat round icon="menu" @click="toggleRightDrawer" />
       </q-toolbar>
     </q-header>
 
-    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
-      <!-- drawer content -->
+    <q-drawer
+      show-if-above
+      v-model="leftDrawerOpen"
+      side="left"
+      bordered
+      :width="283"
+      >
+
+      <q-icon name="fas fa-dove" size="lg" color="primary" class="q-pa-md" />
+
+      <q-item
+        to="/"
+        clickable
+        v-ripple
+      >
+        <q-item-section avatar>
+          <q-icon name="home" size="md" />
+        </q-item-section>
+
+        <q-item-section class="text-h6 text-weight-bold">Home</q-item-section>
+      </q-item>
+
+      <q-item
+        to="/about"
+        clickable
+        v-ripple
+      >
+        <q-item-section avatar>
+          <q-icon name="help" size="md" />
+        </q-item-section>
+
+        <q-item-section class="text-h6 text-weight-bold">About</q-item-section>
+      </q-item>
     </q-drawer>
 
     <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
-      <!-- drawer content -->
+          <q-input
+            class="q-ma-md"
+            rounded
+            outlined
+            placeholder="search the site"
+            dense
+          >
+
+        <template v-slot:prepend>
+          <q-icon name="search" />
+        </template>
+      </q-input>
+
     </q-drawer>
 
     <q-page-container>
       <router-view />
     </q-page-container>
-
   </q-layout>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref } from "vue";
 
 export default {
-  setup () {
-    const leftDrawerOpen = ref(false)
-    const rightDrawerOpen = ref(false)
+  setup() {
+    const leftDrawerOpen = ref(false);
+    const rightDrawerOpen = ref(false);
 
     return {
       leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
       },
 
       rightDrawerOpen,
-      toggleRightDrawer () {
-        rightDrawerOpen.value = !rightDrawerOpen.value
-      }
-    }
-  }
-}
+      toggleRightDrawer() {
+        rightDrawerOpen.value = !rightDrawerOpen.value;
+      },
+    };
+  },
+};
 </script>
